eceda4a01841b704502239ce136509475844c9c3
GTECH\lazar.l <lazarlyutakov@gmail.com>
2019-10-21 15:44:23 +0300

    added asp.net core tutorial repo

---
+++ asp.net-core-fundamentals/OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,40 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Qkata pica", Location = "Sofia", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Pari mi na zadnika", Location = "Sofia", Cuisine = CuisineType.Mexican},
+                new Restaurant {Id = 3, Name = "ne pipai kravata", Location = "Sofia", Cuisine = CuisineType.Indian}
+            };
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
